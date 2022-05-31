e7a316d097859650aa38b248a94aea3e240dd790
HamedPour <hamed_k.pour@hotmail.com>
2022-05-31 13:21:34 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import Landing from './pages/Landing.svelte';
+import TailwindCss from './styles/TailwindCSS.svelte';
+</script>
+
+<TailwindCss/>
+<main>
+  <Landing/>
+</main>
---
+++ src/pages/Landing.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { sayHelloWorld } from "../lib/helloWorld";
+</script>
+
+<div>
+  <h1 class="text-4xl text-center">Landing Page</h1>
+<h3>{sayHelloWorld()}</h3>
+</div>
